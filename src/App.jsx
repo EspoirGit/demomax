6ca6a7cf83c9a97344f65a4dabed9bfb237382a5
EspoirGit <danhouminespoir@gmail.com>
2025-04-12 08:17:47 +0100

    update bouton sur page statistique

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
