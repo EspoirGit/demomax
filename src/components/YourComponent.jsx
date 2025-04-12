d11bc849243c407f0b530cd28d32332c43c3da21
EspoirGit <danhouminespoir@gmail.com>
2025-04-12 01:23:42 +0100

    responsive try

---
+++ src/components/YourComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './YourComponent.css';
+
+const YourComponent = () => {
+  return (
+    <div className="your-component">
+      <h1>Responsive Component</h1>
+      <p>This component adjusts to different screen sizes.</p>
+    </div>
+  );
+};
+
+export default YourComponent;
---
+++ src/pages/YourPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './YourPage.css';
+
+const YourPage = () => {
+  return (
+    <div className="your-page">
+      <h1>Responsive Page</h1>
+      <p>This page adjusts to different screen sizes.</p>
+    </div>
+  );
+};
+
+export default YourPage;
